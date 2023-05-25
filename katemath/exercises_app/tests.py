2c81aa3ccdc06d92883cca95ee380bdec340896c
Cezary <cezary.szukiel@gmail.com>
2023-05-25 12:57:36 +0200

    exercises list test

--- katemath/exercises_app/tests.py
+++ katemath/exercises_app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+import pytest as pytest
+from django.test import Client
+from django.urls import reverse
 
 # Create your tests here.
+
+
+@pytest.mark.django_db
+def test_exercises_list_view():
+    client = Client()
+    url = reverse('exercises_list')
+    response = client.get(url)
+    assert response.status_code == 200
+    # paginacji nie sprawdzamy bo to jest funkcja wbudowana w django
+
