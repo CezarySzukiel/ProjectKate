73d8e4c00e926c28374c781a3425c820cdb03d1b
Cezary <cezary.szukiel@gmail.com>
2023-05-25 12:05:28 +0200

    views.py cleared a bit

--- katemath/exercises_app/views.py
+++ katemath/exercises_app/views.py
@@ -12,26 +12,6 @@ class ExercisesListView(ListView):
     template_name = 'exercises_app/exercises_list_view.html'
     model = Exercises
     paginate_by = 10
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # context['subsections'] = Subsections.objects.all() #todo potrzebne tylko roboczo
-        # context['sections'] = Sections.objects.all() #todo potrzebne tylko roboczo
-        context['sort_by_subsections_form'] = SortBySubsectionsForm()
-        context['sort_by_sections_form'] = SortBySectionsForm()
-        return context
-
-    # def get_queryset(self):
-    #     queryset = super().get_queryset()
-    #     subsection = self.request.GET.get('sort_by_subsections_form', '')
-    #     section = self.request.GET.get('sort_by_sections_form', '')
-    #     if subsection:
-    #         queryset = queryset.filter(subsection__name=subsection)
-    #     if section:
-    #         queryset = queryset.filter(subsection__section__name=section)
-    #     return queryset
-
-
 # jakiego widoku użyć do wyświetlenia zadania, pobrania odpowiedzi i porównania odpowiedzi z rozwiązaniem?
 
 class ExerciseDetailsView(View):
