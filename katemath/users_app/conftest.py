22e5d10a36db6a1517aeb9e951376c1a4eaea14b
Cezary <cezary.szukiel@gmail.com>
2023-05-27 06:34:06 +0200

    test for CreateUserView

--- katemath/users_app/conftest.py
+++ katemath/users_app/conftest.py
@@ -1,1 +1,19 @@
-from.models import import
+import pytest
+from django.contrib.auth.models import User
+
+from .models import UserSettings
+
+
+@pytest.fixture
+def user():
+    return User.objects.create(username='test_user',
+                               email='testuser@wp.pl',
+                               password1='testpassword')
+
+
+@pytest.fixture
+def user_settings():
+    lst = []
+    lst.append(UserSettings.objects.create(user_id=1,
+                                           level=False,
+                                           points=0))
--- katemath/users_app/tests.py
+++ katemath/users_app/tests.py
@@ -2,30 +2,52 @@ from django.test import TestCase
 import pytest
 from django.test import Client
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your tests here.
 
 
-def test_create_user_get():
-    client = Client()
+def test_create_user_get(client):
+    """check if register page is displayed correctly"""
     url = reverse('register')
     response = client.get(url)
     assert response.status_code == 200
 
 
-def test_create_user_get_content():
-    client = Client()
+def test_create_user_get_content(client):
+    """Powtarzam kod, aby mieć 2 testy, można było umieścić 2 asserty w jednym teście"""
     url = reverse('register')
     response = client.get(url)
-    assert 'Username' in response.content.decode()
+    assert 'form' in response.context
 
-def test_create_user_post_valid():
-    client = Client()
+
+@pytest.mark.django_db
+def test_create_user_post_valid_form(client):
+    """check if user is created when form is valid"""
     url = reverse('register')
-    data = {'username': 'test_user',
-            'email': 'testuser@wp.pl',
-            'password1': 'testpassword',
-            'password2': 'testpasword'}
+    data = {
+        'username': 'testuser',
+        'email': 'testuser@wp.pl',
+        'password1': 'testpassword',
+        'password2': 'testpassword'
+    }
     response = client.post(url, data)
     assert response.status_code == 302
-    # todo zrobić fixture z userem
+    assert response.url == reverse('base')
+    assert User.objects.get(username='testuser')
+
+
+@pytest.mark.django_db
+def test_create_user_post_invalid_form(client):
+    """check if user is not created when passwords are different"""
+    url = reverse('register')
+    data = {
+        'username': 'testuser',
+        'password1': 'testpassword',
+        'password2': 'differentpassword'
+    }
+    response = client.post(url, data)
+    assert response.status_code == 200
+    assert 'form' in response.context
+    assert not User.objects.filter(username=data['username']).exists()
+
