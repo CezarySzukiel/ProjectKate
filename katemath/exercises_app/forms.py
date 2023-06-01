d8ec26f8825662b711a2f78b5686a663db5fbff6
Cezary <cezary.szukiel@gmail.com>
2023-06-01 21:08:03 +0200

    form for sorting

--- katemath/exercises_app/forms.py
+++ katemath/exercises_app/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from .models import Exercises, Sections, Subsections
 
 
 class AnswerForm(forms.Form):
     """Form for answer to exercise"""
     answer = forms.CharField(max_length=128, label='Odpowiedź: ')
+
+
+class FilterForm(forms.Form):
+    """Form for filtering exercises"""
+    sections_choices = Sections.objects.all()
+    subsections_choices = Subsections.objects.all()
+    sections = forms.ModelMultipleChoiceField(queryset=sections_choices, widget=forms.CheckboxSelectMultiple)
+    subsections = forms.ModelMultipleChoiceField(queryset=subsections_choices, widget=forms.CheckboxSelectMultiple)
+
